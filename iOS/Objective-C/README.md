8d24d37d06ff5ce6dc68820dad3d6406511fc1a1
Nikan <t-nimogh@microsoft.com>
2019-11-11 13:29:31 +0100

    Added Cosmos DB Integration

--- iOS/Objective-C/README.md
+++
@@ -1,3 +0,0 @@
-# iOS Objective-C Sample
-
-See our [quickstart guide](https://docs.microsoft.com/en-us/azure/spatial-anchors/quickstarts/get-started-ios) for sample instructions.
--- iOS/Objective-C/SampleObjC/BasicDemoViewController.h
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-#import "BaseViewController.h"
-
-@interface BasicDemoViewController : BaseViewController
-{
-}
-@end
