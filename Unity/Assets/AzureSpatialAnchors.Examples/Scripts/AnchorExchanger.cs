dbf11f26f2298094fb141a63ae7d9df1eacc1d8c
Nikan <52320631+nikanmog@users.noreply.github.com>
2020-01-09 17:05:38 +0100

    Bugfixes for V2.2

--- Unity/Assets/AzureSpatialAnchors.Examples/Scripts/AnchorExchanger.cs
+++ Unity/Assets/AzureSpatialAnchors.Examples/Scripts/AnchorExchanger.cs
@@ -10,18 +10,18 @@ namespace Microsoft.Azure.SpatialAnchors.Unity.Examples
 {
     public class AnchorExchanger
     {
-#if !UNITY_EDITOR
+
         private string baseAddress = "";
 
-        private List<string> anchorkeys = new List<string>();
+        public Dictionary<string, int> anchorkeys = new Dictionary<string, int>();
 
-        public List<string> AnchorKeys
+        public Dictionary<string, int> AnchorKeys
         {
             get
             {
                 lock (anchorkeys)
                 {
-                    return new List<string>(anchorkeys);
+                    return new Dictionary<string, int>(anchorkeys);
                 }
             }
         }
@@ -31,22 +31,35 @@ namespace Microsoft.Azure.SpatialAnchors.Unity.Examples
             baseAddress = exchangerUrl;
             Task.Factory.StartNew(async () =>
                 {
-                    string previousKey = string.Empty;
                     while (true)
                     {
-                        string currentKey = await RetrieveLastAnchorKey();
-                        if (!string.IsNullOrWhiteSpace(currentKey) && currentKey != previousKey)
+                        for (int i = 1; 1<5; i++)
                         {
-                            Debug.Log("Found key " + currentKey);
-                            lock (anchorkeys)
+                            string currentKey = await RetrieveAnchorKey(i);
+                            int currentType = await RetrieveAnchorType(i);
+                            if (!string.IsNullOrWhiteSpace(currentKey))
                             {
-                                anchorkeys.Add(currentKey);
+                                Debug.Log("Found key " + currentKey);
+                                lock (anchorkeys)
+                                {
+                                    anchorkeys.Add(currentKey, currentType);
+                                }
                             }
-                            previousKey = currentKey;
+                            await Task.Delay(500);
                         }
                         await Task.Delay(500);
                     }
                 }, TaskCreationOptions.LongRunning);
+        }
+        public int anchorType(string anchorKey)
+        {
+            int defaultType = 1;
+            
+            if (anchorkeys.ContainsKey(anchorKey))
+            {
+                return anchorkeys[anchorKey];
+            }
+            return defaultType;
         }
 
         public async Task<string> RetrieveAnchorKey(long anchorNumber)
@@ -64,6 +77,24 @@ namespace Microsoft.Azure.SpatialAnchors.Unity.Examples
             }
         }
 
+
+        public async Task<int> RetrieveAnchorType(long anchorNumber)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                return int.Parse(await client.GetStringAsync(baseAddress + "/type/" + anchorNumber.ToString()));
+
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                Debug.LogError($"Failed to retrieve anchor key for anchor number: {anchorNumber}.");
+                return 0;
+            }
+        }
+
+
         public async Task<string> RetrieveLastAnchorKey()
         {
             try
@@ -76,6 +107,22 @@ namespace Microsoft.Azure.SpatialAnchors.Unity.Examples
                 Debug.LogException(ex);
                 Debug.LogError("Failed to retrieve last anchor key.");
                 return null;
+            }
+        }
+
+        public async Task<int> RetrieveLastAnchorType()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                
+                return int.Parse(await client.GetStringAsync(baseAddress + "/last/type"));
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                Debug.LogError("Failed to retrieve last anchor key.");
+                return 0;
             }
         }
 
@@ -119,6 +166,6 @@ namespace Microsoft.Azure.SpatialAnchors.Unity.Examples
                 return -1;
             }
         }
-#endif
+
     }
 }
