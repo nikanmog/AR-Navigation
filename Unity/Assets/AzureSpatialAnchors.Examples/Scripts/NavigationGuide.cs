77879179b08385040f9f8f32671fcf7a0fdfaff2
Nikan <52320631+nikanmog@users.noreply.github.com>
2020-01-21 15:55:56 +0100

    Different Animations for different parts of the journey

--- Unity/Assets/AzureSpatialAnchors.Examples/Scripts/NavigationGuide.cs
+++ Unity/Assets/AzureSpatialAnchors.Examples/Scripts/NavigationGuide.cs
@@ -6,6 +6,7 @@ public class NavigationGuide : MonoBehaviour
     #region Object Members
     private ARNavigation arnavigation;
     private GameObject guide = null;
+    private Animation anim;
     private int originId = 1;
     private int destinationId = 2;
     #endregion Object Members
@@ -19,7 +20,7 @@ public class NavigationGuide : MonoBehaviour
         if(arnavigation.allspawnedObjects.Count >= 2)
         {
             spawnGuide();
-            updatePosition();
+            updateBehaviour();
         }
     }
     private void spawnGuide()
@@ -27,47 +28,23 @@ public class NavigationGuide : MonoBehaviour
         if(guide == null)
         {
             guide = GameObject.Instantiate(arnavigation.guidePrefab,guidePosition(), guideDirection());
+            anim = guide.GetComponent<Animation>();
         }
     }
-    private void updatePosition()
-    {
-        guide.transform.position = guidePosition();
-        guide.transform.rotation = guideDirection();
-    }
     #endregion Control Functions
-
-    private GameObject origin() {
+    #region Helper Functions
+    private GameObject origin()
+    {
         return arnavigation.allspawnedObjects[arnavigation.anchorOrder[originId]];
-        
+
     }
     private GameObject destination()
     {
         return arnavigation.allspawnedObjects[arnavigation.anchorOrder[destinationId]];
     }
-    private Vector3 guidePosition()
-    {
-       
-        float multiplicator = 0.0f;
-        float newDistance = 999999.0f;
-        float previousDistance = 99999999.0f;
-        while (newDistance < previousDistance && multiplicator <= 1.0f)
-        {
-            previousDistance = newDistance;
-            newDistance = Vector3.Distance(origin().transform.position + path() * multiplicator, Camera.main.transform.position);
-            multiplicator += 0.01f;
-        }
-        multiplicator *= 1.3f;
-        if (multiplicator >= 1)
-        {
-            moveToNextObject();
-            return origin().transform.position + path() * 1.0f;
-        }
-        //arnavigation.printmsg = "MP:" + multiplicator + " OR:" + originId + "DS" + destinationId;
-        return origin().transform.position + path() * multiplicator;
-    }
     private void moveToNextObject()
     {
-        if(destinationId < arnavigation.anchorOrder.Count)
+        if (destinationId < arnavigation.anchorOrder.Count)
         {
             originId += 1;
             destinationId += 1;
@@ -77,8 +54,58 @@ public class NavigationGuide : MonoBehaviour
     {
         return Quaternion.LookRotation(path());
     }
+    private Quaternion guideToCamera()
+    {
+        
+        return Quaternion.LookRotation(new Vector3(Camera.main.transform.position.x - guide.transform.position.x, 0, Camera.main.transform.position.z - guide.transform.position.z));
+    }
     private Vector3 path()
     {
         return destination().transform.position - origin().transform.position;
     }
+    #endregion Helper Functions
+    private void updateBehaviour()
+    {
+        guide.transform.position = guidePosition();
+        if (originId == 1 && guideProgression() <= 0.1f )
+        { // At start
+            guide.transform.rotation = guideToCamera();
+            anim.Play("0|shake_0");
+        } else if (destinationId == arnavigation.anchorOrder.Count && guideProgression() >= 0.9f)
+        { // At destination
+            guide.transform.rotation = guideToCamera();
+            anim.Play("0|rollover_0");
+        } else
+        { // On the move
+            guide.transform.rotation = guideDirection();
+            anim.Play("0|standing_0");
+        }
+
+    }
+
+    private float guideProgression()
+    {
+        float multiplicator = 0.0f;
+        float newDistance = 999999.0f;
+        float previousDistance = 99999999.0f;
+        while (newDistance < previousDistance && multiplicator <= 1.0f)
+        {
+            previousDistance = newDistance;
+            newDistance = Vector3.Distance(origin().transform.position + path() * multiplicator, Camera.main.transform.position);
+            multiplicator += 0.01f;
+        }
+        return multiplicator;
+    }
+    private Vector3 guidePosition()
+    {
+        float multiplicator = guideProgression() * 1.3f;
+        if (multiplicator >= 1)
+        {
+            moveToNextObject();
+            return origin().transform.position + path() * 1.0f;
+        }
+        //arnavigation.printmsg = "MP:" + multiplicator + " OR:" + originId + "DS" + destinationId;
+        return origin().transform.position + path() * multiplicator;
+    }
+
 }
